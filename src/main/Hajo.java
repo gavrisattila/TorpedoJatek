9312ea67f4ec7fe6bbfe11d700e2045d4a76fdfc
gavrisattila <gavrisa01@gmail.com>
2022-05-23 15:18:28 +0200

    Tesztlövés átnevezése tesztlovesTalaltra

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -5,6 +5,15 @@ public class Hajo {
     private int [] pozíció;
 
     public String talalat(int poz){
-        return "";
+        int i=0;
+        while (i<3 && !(pozíció[i]==poz)) {            
+            i++;
+        }
+        
+        return i<3 ? "talált":"mellé";
+    }
+
+    public Hajo(int[] pozíció) {
+        this.pozíció = pozíció;
     }
 }
--- src/main/Program.java
+++ src/main/Program.java
@@ -5,10 +5,10 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Teszt");
-        assert 1==2 :"nem jó az ellenőrzés";
+        new main.Program().tesztLovesTalalt(4);
     }
-    public String tesztLoves(int poz){
-        Hajo hajo = new Hajo();
+    public String tesztLovesTalalt(int poz){
+        Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(4);
         assert t.equals("talált "):"nem jó a találat";
         return ("");
