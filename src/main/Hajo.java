b07b7f62d93c9fe68bc560873d2450efeea9c8b1
gavrisattila <gavrisa01@gmail.com>
2022-05-23 15:06:26 +0200

    tesztloves() elbukott

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author szf1_a2
- */
 public class Hajo {
-    
+    private int [] pozíció;
+
+    public String talalat(int poz){
+        return "";
+    }
 }
--- src/main/Program.java
+++ src/main/Program.java
@@ -4,7 +4,13 @@ package main;
 public class Program {
 
     public static void main(String[] args) {
-        
+        System.out.println("Teszt");
+        assert 1==2 :"nem jó az ellenőrzés";
     }
-    
+    public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált "):"nem jó a találat";
+        return ("");
+    }
 }
